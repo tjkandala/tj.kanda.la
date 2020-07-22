fc6363c3cce85b39df8b42ea89038a83fe598796
TJ Kandala <kandalatj@gmail.com>
2020-07-21 20:17:40 -0400

    working on custom blog DSL

--- src/parser.ts
+++ src/parser.ts
@@ -3,10 +3,7 @@ import { promisify } from "util";
 import { readFile } from "fs/promises";
 
 /**
- * parses the DSL for .la files
- *
- * .la grammar:
- *
+ * turns a subset of markdown into html files
  */
 
 const getFiles = promisify(readdir);
@@ -18,7 +15,7 @@ type State = "" | "";
 async function main() {
   const filenames = await getFiles(postsDirectory());
 
-  // TODO: use createReadStream! don't keep all files in memory
+  // TODO: use createReadStream! don't keep all files in memory. finish first tho
   const textPromises: Promise<string>[] = filenames.map(name =>
     readFile(postsDirectory(name), "utf8")
   );
@@ -28,12 +25,77 @@ async function main() {
   for (const file of files) {
     console.log(file);
 
-    const tokenizer = createTokenizer(file);
+    const tokens = tokenize(file);
   }
 }
 
 main();
 
-function createTokenizer(source: string) {
-  let state = "";
+/** [type, token] */
+type Token =
+  | ["title", "*title*"]
+  | ["heading", "*header*"]
+  | ["subheading", "*subhead*"]
+  | ["paragraph", "*p*"]
+  | ["bold", "|b|"]
+  | ["italics", "|i|"]
+  | ["link", string];
+
+/**
+ *
+ * grammar of my blog DSL:
+ *
+ * Why?
+ *
+ *
+ * @param source
+ */
+function tokenize(source: string) {
+  const len = source.length;
+  let cursor = 0;
+  let tokens: string[] = [];
+  let char: string;
+
+  while (cursor < len) {
+    char = source[cursor];
+
+    switch (char) {
+      case "*":
+        // lookahead
+        let lookahead = cursor + 1;
+        let possibleKeyword = "";
+        while (lookahead < len) {
+          let nextChar = source[lookahead];
+          possibleKeyword += nextChar;
+          lookahead++;
+          if (nextChar == "*") {
+            break;
+          }
+        }
+
+        switch (possibleKeyword) {
+          default:
+            console.log(possibleKeyword);
+        }
+
+        tokens.push(char);
+        break;
+      case "|":
+        break;
+      default:
+        break;
+    }
+
+    cursor++;
+  }
 }
+
+function parser() {}
+
+function renderer(parseTree: any) {}
+
+/**
+ * exercises:
+ * - write 'isEmail'
+ * - write 'is IPv4'
+ */
